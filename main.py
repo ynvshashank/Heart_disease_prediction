ecb782bdfb4ad4b2e9fe7cc1a125205733176ae3
shashank <97107554+ynvshashank@users.noreply.github.com>
2024-04-15 09:43:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 
